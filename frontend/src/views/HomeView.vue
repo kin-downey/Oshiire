adfde36616d8b391827c99219dfbe6d4b857a522
y-watagashi <kinchan.yamamoto@gmail.com>
2023-12-01 22:52:27 +0900

    [update] Tiptapを使用
    - 試験的にTiptapのマークダウンエディタをコンポーネントとして実装

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Tiptap from '../components/Tiptap.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Tiptap />
   </main>
 </template>
