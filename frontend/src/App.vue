ff4bce399c137ab71cae2d650dfd6d34798de7c2
y-watagashi <kinchan.yamamoto@gmail.com>
2023-12-02 09:22:06 +0900

    [update] ヘッダーの追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/page/2">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <v-app>
+    <Header />
+    <RouterView />
+  </v-app>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <v-app-bar color="primary" density="compact">
+        <v-app-bar-title>Oshiire</v-app-bar-title>
+        <v-btn icon="mdi-plus-box-multiple-outline"></v-btn>
+    </v-app-bar>
+</template>
